7ba99062326472c0c11e4b8b8d36690abe33af46
songhai <doosix@outlook.fr>
2026-09-13 21:15:41 +0200

    updating ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -304,7 +304,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ansible_instance" {
-  ami                    = data.aws_ami.ubuntu
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t4g.micro"
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   subnet_id              = aws_subnet.public.id
