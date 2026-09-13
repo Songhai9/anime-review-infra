50824dba8fd099780246cc848f6e8ccffece7351
songhai <doosix@outlook.fr>
2026-09-13 09:18:04 +0200

    creating nat gateway for api

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,3 +62,20 @@ resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_eip" "ngw_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.ngw_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
