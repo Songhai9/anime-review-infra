31b4890c4f123078b059c58d947728572fdad77d
songhai <doosix@outlook.fr>
2026-09-13 16:45:54 +0200

    fix syntax in route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,7 +107,7 @@ resource "aws_route_table" "db_rt" {
 
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
