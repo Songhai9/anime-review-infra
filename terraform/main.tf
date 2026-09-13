4ab21730a4dae3536728f2e1062512cec123b415
songhai <doosix@outlook.fr>
2026-09-13 09:12:10 +0200

    Fix cidr in route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,8 +18,8 @@ resource "aws_internet_gateway" "igw" {
 
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -49,12 +49,12 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "outbound traffic"
+    Name = "public route table"
   }
 }
 
