dd3f3444000cc3102123f0b225656a1c4f9d690a
songhai <doosix@outlook.fr>
2026-09-14 14:28:33 +0200

    configure outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,23 @@
+output "frontend_public_ip" {
+  value = aws_instance.frontend_instance.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend_instance.public_ip
+}
+
+output "bastion_eip" {
+  value = aws_eip.bastion_eip.public_ip
+}
+
+output "frontend_private_ip" {
+  value = aws_instance.frontend_instance.private_ip
+}
+
+output "backend_private_ip" {
+  value = aws_instance.backend_instance.private_ip
+}
+
+output "bastion_private_ip" {
+  value = aws_instance.ansible_instance.private_ip
+}
