e1db8949cf207a8638f7edf7c5055d0ce443268f
songhai <doosix@outlook.fr>
2026-09-14 15:11:45 +0200

    generation script refinement

--- ansible/scripts/generate_inventory.sh
+++ ansible/scripts/generate_inventory.sh
@@ -4,16 +4,16 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 TERRAFORM_DIR="$SCRIPT_DIR/../../terraform"
 INVENTORY_DIR="$SCRIPT_DIR/../inventory"
 
-cd $TERRAFORM_DIR
+cd "$TERRAFORM_DIR"
 
 BASTION_EIP=$(terraform output -json | jq -r '.bastion_eip.value')
-BASTION_PRIVATE_IP=$(terraform output -json | jq -r '.bastion_private_ip.value')
-FRONTEND_PUBLIC_IP=$(terraform output -json | jq -r '.frontend_public_ip.value')
+# BASTION_PRIVATE_IP=$(terraform output -json | jq -r '.bastion_private_ip.value')
+# FRONTEND_PUBLIC_IP=$(terraform output -json | jq -r '.frontend_public_ip.value')
 FRONTEND_PRIVATE_IP=$(terraform output -json | jq -r '.frontend_private_ip.value')
 BACKEND_PRIVATE_IP=$(terraform output -json | jq -r '.backend_private_ip.value')
 DATABASE_PRIVATE_IP=$(terraform output -json | jq -r '.database_private_ip.value')
 
-cd $INVENTORY_DIR
+cd "$INVENTORY_DIR"
 
 cat > inventory.ini <<EOF
 [bastion]
