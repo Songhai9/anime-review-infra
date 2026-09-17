d8a1c03f6d0688a1186c0281e35780ef9aed75a4
songhai <doosix@outlook.fr>
2026-09-17 15:56:39 +0200

    migrate backend account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "songhai9-anime-review-tfstate"
+    bucket       = "songhai9-anime-review-tfstate-0909-eu-north-1"
     key          = "anime-review/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
